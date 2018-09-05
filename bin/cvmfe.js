41d9f4cfdfa3ed81a31708f82acde1e6d980b087
Conrado Quilles Gomes <conradoqg@gmail.com>
2018-09-05 18:33:16 -0300

    Add CLI to run workers

---
+++ bin/cvmfe.js
@@ -0,0 +1,49 @@
+const path = require('path');
+if (process.argv[1].includes('snapshot')) process.argv[1] = process.argv[1].replace('arte.js', path.relative(process.cwd(), process.argv0)); // Workaround that shows the correct file path inside the pkg generated file
+const yargs = require('yargs');
+const cvmDataProcess = require('../lib/cvmDataProcess');
+const cvmStatisticProcess = require('../lib/cvmStatisticProcess');
+
+const createCommandHandler = (func) => {
+    return async (argv) => {
+        try {
+            await func(argv);
+            process.exit(0);
+        }
+        catch (ex) {
+            if (ex.toPrint) console.error(ex.toPrint());
+            else console.error(ex.stack);
+            process.exit(1);
+        }
+    };
+};
+
+yargs
+    .example('$0 run cvmDataProcess', 'Download, convert and insert data from CVM to database.')
+    .example('$0 run cvmStatisticProcess -b', 'Load CVM data from database and generate financial information.')
+
+    .command('run <workerName> [options]', 'run a worker', (yargs) => {
+        return yargs
+            .positional('workerName', {
+                alias: 'worker',
+                describe: 'worker name (cvmDataProcess, cvmStatisticProcess, all)'
+            })
+            .version(false);
+    }, createCommandHandler(async (argv) => {
+        const worker = argv.worker;
+
+        if (worker.toLowerCase() == 'cvmDataProcess') {
+            return await cvmDataProcess();
+        } else if (worker.toLowerCase() == 'cvmStatisticProcess') {
+            return await cvmStatisticProcess();
+        } else if (worker.toLowerCase() == 'all') {
+            await cvmDataProcess();
+            return await cvmStatisticProcess();
+        }
+    }))
+    .demandCommand(1)
+    .version()
+    .help('h')
+    .alias('h', 'help')
+    .wrap(yargs.terminalWidth())
+    .argv;
