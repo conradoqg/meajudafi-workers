3e345222101cc75173eae02267e6c223f5858718
Conrado Quilles Gomes <conradoqg@gmail.com>
2018-11-09 16:33:07 -0200

    Change build script to allow local build

--- build.sh
+++ build.sh
@@ -2,15 +2,23 @@
 
 IMAGE=cvm-fund-explorer-workers
 
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker build -t $IMAGE .
 
-docker tag $IMAGE $DOCKER_USERNAME/$IMAGE:$TRAVIS_BRANCH
-docker push $DOCKER_USERNAME/$IMAGE:$TRAVIS_BRANCH
+if [ -n "$TRAVIS_BRANCH" ]; then
+    docker tag $IMAGE $DOCKER_USERNAME/$IMAGE:$TRAVIS_BRANCH
+    docker push $DOCKER_USERNAME/$IMAGE:$TRAVIS_BRANCH
 
-if [ "$TRAVIS_BRANCH" = "master" ]; then
-    docker tag $IMAGE $DOCKER_USERNAME/$IMAGE:latest
-    docker push $DOCKER_USERNAME/$IMAGE:latest
+    if [ "$TRAVIS_BRANCH" = "master" ]; then
+        echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+        docker tag $IMAGE $DOCKER_USERNAME/$IMAGE:latest
+        docker push $DOCKER_USERNAME/$IMAGE:latest
+    fi
+else
+    BRANCH=$(git rev-parse --abbrev-ref HEAD)
+    docker tag $IMAGE conradoqg/$IMAGE:$BRANCH
+    if [ "$BRANCH" = "master" ]; then
+        docker tag $IMAGE conradoqg/$IMAGE:latest
+    fi
 fi
 
 docker images
