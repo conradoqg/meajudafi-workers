2432d93589475b4083ab3cc059471da6404093dc
Conrado Quilles Gomes <conradoqg@gmail.com>
2018-12-02 16:17:54 -0200

    Add CDI benchmark to the daily investment table

---
+++ migrations/0000005-field-cdi_investment_return.down.sql
@@ -0,0 +1,2 @@
+ALTER TABLE public.investment_return_daily
+    DROP COLUMN cdi_investment_return;
---
+++ migrations/0000005-field-cdi_investment_return.up.sql
@@ -0,0 +1,2 @@
+ALTER TABLE public.investment_return_daily
+    ADD COLUMN cdi_investment_return double precision;
