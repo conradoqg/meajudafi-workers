896752e5b589611b936860b3853f6915040d0a53
Conrado Quilles Gomes <conradoqg@gmail.com>
2018-12-26 08:41:29 -0200

    Fix migration 0000009

--- migrations/0000009-investment_return_bovespa_fields_and_general_renaming.up.sql
+++ migrations/0000009-investment_return_bovespa_fields_and_general_renaming.up.sql
@@ -164,7 +164,7 @@ SELECT
 	icf_classe, 
 	last_day_of_month as irm_dt_comptc, 
 	COALESCE(irm_accumulated_investment_return,0) as irm_accumulated_investment_return, 
-	COALESCE(irm_cdi_accumulated_risk,0) as irm_cdi_accumulated_risk 
+	COALESCE(irm_accumulated_risk,0) as irm_accumulated_risk 
 	FROM (SELECT * FROM xpi_funds
 			LEFT JOIN inf_cadastral_fi_fullname ON xf_cnpj = icf_cnpj_fundo
 			CROSS JOIN (SELECT CAST(date_trunc('month',ts) + interval '1 month - 1 day' AS DATE) AS last_day_of_month
