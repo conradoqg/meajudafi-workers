d0b730a7453206371a7b5501cd55546559a32e79
Conrado Quilles Gomes <conradoqg@gmail.com>
2019-01-03 11:53:46 -0200

    Reorganize workers and improve logging

--- bin/cvmfe.js
+++ bin/cvmfe.js
@@ -1,10 +1,11 @@
 const path = require('path');
 if (process.argv[1].includes('snapshot')) process.argv[1] = process.argv[1].replace('arte.js', path.relative(process.cwd(), process.argv0)); // Workaround that shows the correct file path inside the pkg generated file
 const yargs = require('yargs');
-const cvmDataProcess = require('../lib/cvmDataProcess');
-const cvmStatisticProcess = require('../lib/cvmStatisticProcess');
-const XPIFundProcess = require('../lib/xpiFundProcess');
-const Db = require('../lib/util/db');
+
+const cvmDataWorker = require('../lib/worker/cvmDataWorker');
+const cvmStatisticWorker = require('../lib/worker/cvmStatisticWorker');
+const XPIFundWorker = require('../lib/worker/xpiFundWorker');
+const MigrateWorker = require('../lib/worker/migrateWorker');
 
 const createCommandHandler = (func) => {
     return async (argv) => {
@@ -21,41 +22,33 @@ const createCommandHandler = (func) => {
 };
 
 yargs
-    .example('$0 run cvmDataProcess', 'Download, convert and insert data from CVM to database.')
-    .example('$0 run cvmStatisticProcess -b', 'Load CVM data from database and generate financial information.')
-    .example('$0 run xpiFundProcess', 'Load XPI data.')
-    .example('$0 run migrate', 'Migrate database.')
+    .example('$0 run cvmDataWorker', 'Download, convert and insert data from CVM to database.')
+    .example('$0 run cvmStatisticWorker -b', 'Load CVM data from database and generate financial information.')
+    .example('$0 run xpiFundWorker', 'Load XPI data.')
+    .example('$0 run migrateWorker', 'Migrate database.')
 
     .command('run <workerName> [options...]', 'run a worker', (yargs) => {
         return yargs
             .positional('workerName', {
                 alias: 'worker',
-                describe: 'worker name (cvmDataProcess, cvmStatisticProcess, all)'
+                describe: 'worker name (cvmDataWorker, cvmStatisticWorker, xpiFundWorker, all)'
             })
             .version(false);
     }, createCommandHandler(async (argv) => {
         const worker = argv.worker;
 
-        if (worker.toLowerCase() == 'cvmDataProcess'.toLowerCase()) {
-            return await cvmDataProcess();
-        } else if (worker.toLowerCase() == 'cvmStatisticProcess'.toLowerCase()) {
-            return await cvmStatisticProcess();
-        } else if (worker.toLowerCase() == 'xpiFundProcess'.toLowerCase()) {
-            return await (new XPIFundProcess()).work();
-        } else if (worker.toLowerCase() == 'migrate'.toLowerCase()) {
-            const db = new Db();
-            if (argv.options[0] == 'rollback') {
-                await db.rollback(argv.options[1]);
-            } else if (argv.options[0] == 'reset') {
-                await db.reset();
-            } else {
-                await db.migrate();
-            }
-            console.log('Migration completed');
+        if (worker.toLowerCase() == 'cvmDataWorker'.toLowerCase()) {
+            await (new cvmDataWorker()).work(argv);
+        } else if (worker.toLowerCase() == 'cvmStatisticWorker'.toLowerCase()) {
+            await (new cvmStatisticWorker()).work(argv);
+        } else if (worker.toLowerCase() == 'xpiFundWorker'.toLowerCase()) {
+            await (new XPIFundWorker()).work(argv);
+        } else if (worker.toLowerCase() == 'migrateWorker'.toLowerCase()) {
+            await (new MigrateWorker()).work(argv);
         } else if (worker.toLowerCase() == 'all'.toLowerCase()) {
-            await cvmDataProcess();
-            await cvmStatisticProcess();
-            await (new XPIFundProcess()).work();
+            await (new cvmDataWorker()).work(argv);
+            await (new cvmStatisticWorker()).work(argv);
+            await (new XPIFundWorker()).work(argv);
         }
     }))
     .demandCommand(1)
