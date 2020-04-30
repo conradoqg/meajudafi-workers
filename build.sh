dd16019c4144b3788e43a45d9ffbe9422b2b9449
Conrado Quilles Gomes <conradoqg@gmail.com>
2020-04-30 20:02:11 -0300

    Prepare for production test

--- build.sh
+++ build.sh
@@ -8,8 +8,8 @@ docker build -t $IMAGE .
 
 if [ -n "$TRAVIS_BRANCH" ]; then
     echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-    docker tag $IMAGE $DOCKER_USERNAME/$IMAGE:${TRAVIS_BRANCH//[\/ ]/_}
-    docker push $DOCKER_USERNAME/$IMAGE:${TRAVIS_BRANCH//[\/ ]/_}
+    docker tag $IMAGE $DOCKER_USERNAME/$IMAGE:${TRAVIS_BRANCH//[\/ ]/-}
+    docker push $DOCKER_USERNAME/$IMAGE:${TRAVIS_BRANCH//[\/ ]/-}
 
     if [ "$TRAVIS_BRANCH" = "master" ]; then        
         docker tag $IMAGE $DOCKER_USERNAME/$IMAGE:latest
@@ -17,7 +17,7 @@ if [ -n "$TRAVIS_BRANCH" ]; then
     fi
 else
     BRANCH=$(git rev-parse --abbrev-ref HEAD)
-    docker tag $IMAGE conradoqg/$IMAGE:${BRANCH//[\/ ]/_}
+    docker tag $IMAGE conradoqg/$IMAGE:${BRANCH//[\/ ]/-}
     if [ "$BRANCH" = "master" ]; then
         docker tag $IMAGE conradoqg/$IMAGE:latest
     fi
