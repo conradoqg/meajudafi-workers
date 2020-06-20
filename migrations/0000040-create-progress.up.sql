06c1b41325fcf2bbb7e7e5cb7512918f9f8ce297
Conrado Quilles Gomes <conradoqg@gmail.com>
2020-06-20 18:21:48 -0300

    Create progress classes and adapt model and btg workers to it

---
+++ migrations/0000040-create-progress.up.sql
@@ -0,0 +1,7 @@
+CREATE UNLOGGED TABLE public.progress (
+	"path" ltree NOT NULL,
+	"data" jsonb NULL,
+	CONSTRAINT progress_pkey PRIMARY KEY ("path")
+);
+CREATE INDEX path_gist_idx ON public.progress ("path");
+CREATE INDEX path_idx ON public.progress ("path");
